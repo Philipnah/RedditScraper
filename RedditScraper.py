f78dd78d646b949d04c85b67a4f4cc031fd8ed23
Philipnah <Philip@han.dk>
2019-12-15 17:54:44 +0100

    getting unauthorized error

--- RedditScraper.py
+++ RedditScraper.py
@@ -2,6 +2,8 @@ import requests
 from lxml import html
 from bs4 import BeautifulSoup as bs
 import getpass
+from time import sleep
+
 
 Username = input("Username: ")
 Password = getpass.getpass()
@@ -18,7 +20,6 @@ def login(Username, Password):
     result = session_requests.get(loginURL)
 
     tree = html.fromstring(result.text)
-    print(result.text)
     authenticity_token = list(set(tree.xpath("//input[@name='csrf_token']/@value")))[0]
 
     # making payload
@@ -41,8 +42,15 @@ def login(Username, Password):
 def getHeaders():
     result = session_requests.get(redditURL)
     soup = bs(result.text, "html.parser")
+    print(soup.prettify())
 
-    print(soup.find_all("h3", class_="_eYtD2XCVieq6emjKBH3m"), result.ok)
+    print(soup.find_all("h3", class_="_eYtD2XCVieq6emjKBH3m"), result.status_code)
 
 login(Username, Password)
+sleep(1)
 getHeaders()
+
+
+# note: if we dont login in, i.e. input is wrong, then the shit works but if we do log in we get jack shit website.
+
+# note: now i added 1 second time delay between functions and now we get error 401 which means unauthorized access.
